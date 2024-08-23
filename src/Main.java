748f307ec4029e091bb147b2b57f31853732e500
sandramarcela987 <sandramarcela987@gmail.com>
2024-08-23 09:59:35 -0500

    cambio sandra2

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,7 @@ public class Main {
         System.out.println("11. Salir");
         System.out.println("*******************************");
         System.out.println("*****************************2*");
-        System.out.println("*****************************3*")
+        System.out.println("*****************************3*");
+        System.out.println("**************************sandra*")
     }
 }
