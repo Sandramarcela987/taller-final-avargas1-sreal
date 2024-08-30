d7a28090d741af110ce666f3debd593315d0a2f0
AlexAntonny <varalex@gmail.com>
2024-08-30 15:55:12 -0500

    ajuste menu

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("******************************* ****");
-        System.out.println("      Taller Programacion          *");
         System.out.println("************************************");
-        System.out.println("             MENU                  *");
+        System.out.println("|      Taller Programación         |");
         System.out.println("************************************");
-        System.out.println("* 1. Datos primitivos              *");
-        System.out.println("* 2. String                        *");
-        System.out.println("* 3. Constantes                    *");
-        System.out.println("* 4. Tipo de operadores            *");
-        System.out.println("* 5. Condicional: IF, ELSE,IF ELSE *");
-        System.out.println("* 6. Condicional Switch            *");
-        System.out.println("* 7. Condicioanl ternaria          *");
-        System.out.println("* 8. Bucle Do While                *");
-        System.out.println("* 9. Bucle While                   *");
-        System.out.println("* 10. Bucle For                    *");
-        System.out.println("* 11. Salir                        *");
+        System.out.println("|            MENU                  |");
         System.out.println("************************************");
-
-
+        System.out.println("| 1. Datos primitivos              |");
+        System.out.println("| 2. String                        |");
+        System.out.println("| 3. Constantes                    |");
+        System.out.println("| 4. Tipo de operadores            |");
+        System.out.println("| 5. Condicional: IF,ELSE,IF ELSE  |");
+        System.out.println("| 6. Condicional Switch            |");
+        System.out.println("| 7. Condicioanal ternaria         |");
+        System.out.println("| 8. Bucle Do While                |");
+        System.out.println("| 9. Bucle While                   |");
+        System.out.println("| 10. Bucle For                    |");
+        System.out.println("| 11. Salir                        |");
+        System.out.println("************************************");
 
 
     }
