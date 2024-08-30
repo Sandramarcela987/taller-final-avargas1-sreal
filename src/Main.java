7db432b9bebd52dd8cfc1172baa01b906c2a1ab7
sandramarcela987 <sandramarcela987@gmail.com>
2024-08-30 15:42:55 -0500

    menu

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("*******************************");
-        System.out.println("    Taller Programacion        ");
-        System.out.println("*******************************");
-        System.out.println("          MENU                 ");
-        System.out.println("*******************************");
-        System.out.println("1. Datos primitivos");
-        System.out.println("2. String");
-        System.out.println("3. Constantes");
-        System.out.println("4. Tipo de operadores");
-        System.out.println("5. Condicional: IF, ELSE, IF ELSE");
-        System.out.println("6. Condicional Switch");
-        System.out.println("7. Condicioanl ternaria");
-        System.out.println("8. Bucle Do While");
-        System.out.println("9. Bucle While");
-        System.out.println("10. Bucle For");
-        System.out.println("11. Salir");
-        System.out.println("*******************************");
-        System.out.println("*****************************2*");
-        System.out.println("*****************************3*");
-        System.out.println("**************************sandra*");
-        System.out.println("**************************marcela*");
-        System.out.println("**************************Alex*");
-        System.out.println("**************************SandrayAlex*");
-        System.out.println("**************************Nuevo Cambio Alex*");
-        System.out.println("**************************cambio sandra");
-        System.out.println("**************************cambio ensayo pullu")
+        System.out.println("******************************* ****");
+        System.out.println("      Taller Programacion          *");
+        System.out.println("************************************");
+        System.out.println("             MENU                  *");
+        System.out.println("************************************");
+        System.out.println("* 1. Datos primitivos              *");
+        System.out.println("* 2. String                        *");
+        System.out.println("* 3. Constantes                    *");
+        System.out.println("* 4. Tipo de operadores            *");
+        System.out.println("* 5. Condicional: IF, ELSE,IF ELSE *");
+        System.out.println("* 6. Condicional Switch            *");
+        System.out.println("* 7. Condicioanl ternaria          *");
+        System.out.println("* 8. Bucle Do While                *");
+        System.out.println("* 9. Bucle While                   *");
+        System.out.println("* 10. Bucle For                    *");
+        System.out.println("* 11. Salir                        *");
+        System.out.println("************************************");
+
 
 
 
