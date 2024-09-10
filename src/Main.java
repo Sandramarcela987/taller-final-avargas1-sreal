743683cd8152d956d766b2fe95b8011cc71f0bec
AlexAntonny <varalex@gmail.com>
2024-09-10 17:33:16 -0500

    submenu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        int menu=0;
+        int salir=0;
+        do {
         System.out.println("************************************");
         System.out.println("|      Taller Programación         |");
         System.out.println("************************************");
@@ -19,6 +24,53 @@ public class Main {
         System.out.println("| 11. Salir                        |");
         System.out.println("************************************");
 
+
+
+        System.out.println("Seleccione la opción que desea consultar");
+        Scanner scanner1 = new Scanner(System.in);
+        int number = scanner1.nextInt();
+
+
+            switch (number) {
+                case 1:
+                    System.out.println("Datos primitivos");
+                    System.out.println("explicacion dato primario");
+                    scanner1.nextLine();
+                    scanner1.nextLine();
+
+                    break;
+                case 2:
+                    System.out.println("String");
+
+                    scanner1.nextLine();
+                    scanner1.nextLine();
+                    break;
+                case 3:
+                    submenu();
+                    System.out.println("Constantes");
+                    scanner1.nextLine();
+                    scanner1.nextLine();
+                    break;
+                case 4:
+                    System.out.println("Tipo de operadores");
+                    break;
+                case 5:
+                    System.out.println("Tipo de operadores");
+                    break;
+                case 11:
+                    salir=1;
+                    break;
+            }
+        }
+        while (salir==0);
+    }
+
+    public static String submenu() {
+        System.out.println("************************************");
+        System.out.println("|      SUBMENU                     |");
+        System.out.println("************************************");
+
+        return "prueba";
 
     }
 }
